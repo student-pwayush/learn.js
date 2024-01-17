7b707aa5a2928f548123b65592a3dd3b309d2f0b
Ayush chandel <147191816+student-pwayush@users.noreply.github.com>
2024-01-17 10:50:33 +0000

    08_String.js

--- 08_String.js
+++ 08_String.js
@@ -1,35 +1,68 @@
-const name = "hitesh"
+const name = "Ayush"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('Ayush-hc-com')
 
-// console.log(gameName[0]);
+console.log(gameName[3]);
 // console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
+// yah ham logo ko word kitna bda hai uski length dekhna k liya haota hai(isme ham logo ka 0 se start hota hai)
+
+
 // console.log(gameName.toUpperCase());
+//ya letter captial mein likna k liya use hota hai 
+
+
 console.log(gameName.charAt(2));
+// ise ham log pta karte hai kon sa word hai 2 no. pra hai 
+
+
 console.log(gameName.indexOf('t'));
+// ise pta hota hai s letter kon se no par hai 
+
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
+// isme ham log pta karte hai 0, 4 k bich mein kon se no. aate hai 
+
 
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
+// isme ham logo ko puce se no leta hai 
 
-const newStringOne = "   hitesh    "
+
+const newStringOne = "   Ayush    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
+// trim se ham log gap kahatam karte hai
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+
+const url = "https://Ayush.com/Ayush%20chandel"
 
 console.log(url.replace('%20', '-'))
+// isme ham log %20 ko replace karte hai - se
 
 console.log(url.includes('sundar'))
+// ise pata karte hai yaa  word hai isme ya nahi 
 
 console.log(gameName.split('-'));
+//ise ham log letter ko alalg kar sakte hai 
+
+
+const words = gameName.split('');
+console.log(words[1]);
+// Expected output: "fox"
+
+const chars = gameName.split('');
+console.log(chars[9]);
+// Expected output: "k"
+
+const strCopy = gameName.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
--- 09_Nums&Maths.js
+++ 09_Nums&Maths.js
@@ -1,34 +1,63 @@
+// ++++++++++++++++++++ NUMS  ++++++++++
 const score = 400
-// console.log(score);
+//console.log(score);
 
-const balance = new Number(100)
-// console.log(balance);
+const balance = new Number(500)
+//console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+// isme ham logo ki digit ki length show karayega 
 
-const otherNumber = 123.8966
+//console.log(balance.toFixed(2));
+// isme ham log tofixed ki value badhayenge utna hi .00 ki value badegi
 
-// console.log(otherNumber.toPrecision(4));
+const anotherString = new Number(452.485)
 
-const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+//console.log(anotherString.toPrecision(5));
+// isne ham logo ki 5 value ki aprox jo ki 8 hai usko 9 lia hai  tak ki digit li 
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+const hundreds = 100000000
+//console.log(hundreds.toLocaleString());
+//isme ham logo ko pta chalta hai kitna zero lage hai (jo ki videshi duniye ki tarah show karenge)
+//console.log(hundreds.toLocaleString('en-IN'));
+// isme ham logo ko pta chalta hai kitna zero lage hai  uke lie 'en-IN' laga denga (jo india digfit ki tarah show karenge)
+
+//  abhi in sabhi consloe.log ko comments kar diye hai jisme problems na hi aage 
+
+// +++++++++++++++++++++++++    MATHS   +++++++++++
+
+console.log(Math);
+// isme ham logo ki bohat bvalue hai jisa ki object , [array], {} etc 
 
 // console.log(Math);
 // console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
+// isme ham log ka sign change hoga bs baki value vahi rahegi
+
+//console.log(Math.round(4.6));
+//isme ham logo ki value 5 aayegi roung value lega 
+
+//console.log(Math.ceil(5.2));
+// isme ham logo ka sidhe 5.2 hai toh 6 lega 
+
 // console.log(Math.floor(4.9));
+// agar ham logo ki value 4 hai toh voo uthna na hi lega jitna hai . ki value nahi leta hai 
+
 // console.log(Math.min(4, 3, 6, 8));
+//sime choti value leta hai 
+
 // console.log(Math.max(4, 3, 6, 8));
+//isme badi value leta hain
 
 console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// isme ham logo ki value always 0 se 1 ki bich mein aayegi aur always change karegi
 
-const min = 10
-const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+//console.log((Math.random()*10) + 1);
+//console.log(Math.floor(Math.random()*10) + 1);
+
+//const min = 10
+//const max = 20
+
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
